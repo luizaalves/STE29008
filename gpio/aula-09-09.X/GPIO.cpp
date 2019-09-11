e8c76693364baacafd316257453dfb99f267deec
Felipe Cardoso <felipepassoscardoso@gmail.com>
2019-09-11 00:13:19 -0300

    Métodos get e set alterados pra trabalhar com ponteiros

--- gpio/aula-09-09.X/GPIO.cpp
+++ gpio/aula-09-09.X/GPIO.cpp
@@ -1,104 +1,180 @@
-/* 
- * File:   GPIO.cpp
- * Author: aluno
- * 
- * Created on 9 de Setembro de 2019, 08:03
+/*
+ * GPIO.cpp
+ *
+ *  Created on: 22 de mar de 2017
+ *      Author: aluno
  */
 
-#include "GPIO.h"
 #include <avr/io.h>
+#include "GPIO.h"
 
-GPIO::GPIO() {
-}
-
-GPIO::GPIO(uint8_t id, PortDirection_t dir) {
+GPIO::GPIO(uint8_t id, PortDirection_t dir){
     _id = id;
-    switch(_id){
-        case 0:
-        case 1: {
-            _bit = _id;
-            if(dir == GPIO.INPUT){
-                DDRE &= ~(1<<_bit); 
-            }
-            else {
-                DDRE |= (1<<_bit); 
-            }
-        }
-        break;
-        case 2:
-        case 3: {
-            _bit = _id+2;
-            if(dir == GPIO.INPUT){
-                DDRE &= ~(1<<_bit); 
-            }
-            else {
-                DDRE |= (1<<_bit); 
-            }
-        }
-        break;
-        case 4: {
-            _bit = _id+1;
-            if(dir == GPIO.INPUT){
-                DDRG &= ~(1<<_bit); 
-            }
-            else {
-                DDRG |= (1<<_bit); 
-            }
-        }  
-        break;
-        case 5: {
-            _bit = _id-2;
-            if(dir == GPIO.INPUT){
-                DDRE &= ~(1<<_bit); 
-            }
-            else {
-                DDRE |= (1<<_bit); 
-            }
-        } 
-        break;
-        case 6:
-        case 7:
-        case 8:
-        case 9: {
-            _bit = _id-3;
-            if(dir == GPIO.INPUT){
-                DDRH &= ~(1<<_bit); 
-            }
-            else {
-                DDRH |= (1<<_bit); 
-            }
-        }   
-        break;
-        case 10:
-        case 11:
-        case 12:
-        case 13: {
-            _bit = _id-6;
-            if(dir == GPIO.INPUT){
-                DDRB &= ~(1<<_bit); 
-            }
-            else {
-                DDRB |= (1<<_bit); 
-            }
-        }
-        break;   
-    }
-    
-}
+	switch (_id) {
+	case 0:
+	case 1:
+		_bit = id;
+		if (dir)
+			DDRE |= (1 << _bit);
+		else
+			DDRE &= ~(1 << _bit);
+		break;
 
-GPIO::~GPIO() {
-}
+	case 2:
+	case 3:
+		_bit = id + 2;
+		if (dir)
+			DDRE |= (1 << _bit);
+		else
+			DDRE &= ~(1 << _bit);
+		break;
 
-bool get(){
-    
-}
-void set(bool val = 1){
-    
-}
-void clear(){
+	case 4:
+		_bit = DDG5;
+		if (dir)
+			DDRG |= (1 << _bit);
+		else
+			DDRG &= ~(1 << _bit);
+		break;
 
-}
-void toggle(){
+	case 5:
+		_bit = DDE3;
+		if (dir)
+			DDRE |= (1 << _bit);
+		else
+			DDRE &= ~(1 << _bit);
+		break;
+
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+		_bit = id - 3;
+		if (dir)
+			DDRH |= (1 << _bit);
+		else
+			DDRH &= ~(1 << _bit);
+		break;
+
+	case 10:
+	case 11:
+	case 12:
+	case 13:
+		_bit = id - 6;
+		if (dir)
+			DDRB |= (1 << _bit);
+		else
+			DDRB &= ~(1 << _bit);
+		break;
+
+	}
 
 }
 
+GPIO::~GPIO() {}
+
+bool GPIO::get() {
+	switch (_id) {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 5:
+		return (bool) PINE & (1 << _bit);
+
+	case 4:
+		return (bool) PING & (1 << _bit);
+
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+		return (bool) PINH & (1 << _bit);
+
+	case 10:
+	case 11:
+	case 12:
+	case 13:
+		return (bool) PINB & (1 << _bit);
+	}
+    return false;
+}
+
+void GPIO::set(bool val) {
+	switch (_id) {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 5:
+		if (val)
+			PORTE |= (1 << _bit);
+		else
+			PORTE &= ~(1 << _bit);
+		break;
+
+	case 4:
+		if (val)
+			PORTG |= (1 << _bit);
+		else
+			PORTG &= ~(1 << _bit);
+		break;
+
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+		if (val)
+			PORTH |= (1 << _bit);
+		else
+			PORTH &= ~(1 << _bit);
+		break;
+
+	case 10:
+	case 11:
+	case 12:
+	case 13:
+		if (val)
+			PORTB |= (1 << _bit);
+		else
+			PORTB &= ~(1 << _bit);
+		break;
+
+	}
+
+}
+
+void GPIO::clear() {
+	this->set(0);
+}
+
+void GPIO::toggle() {
+	switch (_id) {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 5:
+		PINE = (1 << _bit);
+		break;
+
+	case 4:
+		PING = (1 << _bit);
+		break;
+
+	case 6:
+	case 7:
+	case 8:
+	case 9:
+		PINH = (1 << _bit);
+		break;
+
+	case 10:
+	case 11:
+	case 12:
+	case 13:
+		PINB = (1 << _bit);
+		break;
+
+	}
+}
