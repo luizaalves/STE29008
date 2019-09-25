1db379c1612657cdde083c9fcb5126672b736ee7
Felipe Cardoso <felipepassoscardoso@gmail.com>
2019-09-25 09:22:23 -0300

    Inicio implementação timer

---
+++ avr/timer/timer.X/Timeout.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   Timeout.cpp
+ * Author: aluno
+ * 
+ * Created on 25 de Setembro de 2019, 08:10
+ */
+
+#include "Timeout.h"
+
+Timeout::Timeout() {
+}
+
+Timeout::~Timeout() {
+    
+}
+
+Timeout::config(uint32_t interval, CALLBACK_t callback){
+    
+    
+}
---
+++ avr/timer/timer.X/main.cpp
@@ -0,0 +1,41 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "GPIO.h"
+#include "Timer.h"
+ 
+ 
+GPIO p2(2, GPIO::OUTPUT);
+GPIO p3(3, GPIO::OUTPUT);
+GPIO p4(4, GPIO::OUTPUT);
+GPIO p5(5, GPIO::OUTPUT);
+ 
+void timeout2_handler(void){
+	p2.toggle();
+}
+ 
+void timeout3_handler(void){
+	p3.toggle();
+}
+ 
+void timeout4_handler(void){
+	p4.toggle();
+}
+ 
+void timeout5_handler(void){
+	p5.toggle();
+}
+ 
+ 
+Timer t = Timer(1000);
+ 
+int main(){
+ 
+	sei();
+	t.addTimeout(1000, &timeout2_handler);
+	t.addTimeout(2000, &timeout3_handler);
+	t.addTimeout(3000, &timeout4_handler);
+	t.addTimeout(4000, &timeout5_handler);
+	while(true){
+		t.timeoutManager();
+	}
+}
