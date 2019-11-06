d02b106bc7b1ef7eb2d3299a12fbe12d632f9044
Felipe Cardoso <felipepassoscardoso@gmail.com>
2019-11-06 02:02:29 -0300

    Erro single conversion corrigido

--- Atividade-4/adc_v2.X/main.cpp
+++ Atividade-4/adc_v2.X/main.cpp
@@ -20,17 +20,14 @@ UART uart(9600, UART::DATABITS_8, UART::NONE, UART::STOPBIT_1);
 ADConverter adc = ADConverter(ADConverter::AVCC, ADConverter::DIV_128);
 char value[sizeof (uint16_t)];  //armazenar caracteres referentes ao sensorValue
 
-int main(int argc, char** argv) {
-     
-    //int0.enable(); //Habilita interrupção externa INT0
+int main(int argc, char** argv) {   
     sei();                      //Ativa interrupção global
     uart.puts("Bloqueante");
     for(int i=0;i < 10;i++){
         uint16_t single = adc.single_read(ADConverter::A0);
         tx_serial(single); //Bloqueante
         _delay_ms(1000);
-    }
-    
+    }   
     uart.puts("Free Running");
     adc.read(ADConverter::A0,50,ADConverter::FREE_RUN);
     for (;;) {
